fefc3d4220803b468384fbe528f8a8abe5c8b9d1
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2017-02-24 17:34:11 +0100

    TIP-694: tests on metric

--- src/Pim/Bundle/CatalogBundle/tests/integration/Completeness/AttributeType/MetricAttributeTypeCompletenessIntegration.php
+++ src/Pim/Bundle/CatalogBundle/tests/integration/Completeness/AttributeType/MetricAttributeTypeCompletenessIntegration.php
@@ -3,6 +3,7 @@
 namespace Pim\Bundle\CatalogBundle\tests\integration\Completeness\AttributeType;
 
 use Pim\Bundle\CatalogBundle\tests\integration\Completeness\AbstractCompletenessPerAttributeTypeIntegration;
+use Pim\Component\Catalog\AttributeTypes;
 
 /**
  * Checks that the completeness has been well calculated for metric attribute type.
@@ -15,6 +16,117 @@ class MetricAttributeTypeCompletenessIntegration extends AbstractCompletenessPer
 {
     public function testMetric()
     {
-        //
+        $family = $this->createFamilyWithRequirement(
+            'another_family',
+            'ecommerce',
+            'a_metric',
+            AttributeTypes::METRIC
+        );
+
+        $this->configureMetricFamilyForAttribute('a_metric', 'Length');
+
+        $productFull = $this->createProductWithStandardValues(
+            $family,
+            'product_full',
+            [
+                'values' => [
+                    'a_metric' => [
+                        [
+                            'locale' => null,
+                            'scope'  => null,
+                            'data'   => ['amount' => 12, 'unit' => 'METER'],
+                        ],
+                    ],
+                ],
+            ]
+        );
+        $this->assertComplete($productFull);
+
+        $productZeroAmount = $this->createProductWithStandardValues(
+            $family,
+            'product_full',
+            [
+                'values' => [
+                    'a_metric' => [
+                        [
+                            'locale' => null,
+                            'scope'  => null,
+                            'data'   => ['amount' => 0, 'unit' => 'METER'],
+                        ],
+                    ],
+                ],
+            ]
+        );
+        $this->assertComplete($productZeroAmount);
+    }
+
+    public function testIncompleteMetric()
+    {
+        $family = $this->createFamilyWithRequirement(
+            'another_family',
+            'ecommerce',
+            'a_metric',
+            AttributeTypes::METRIC
+        );
+
+        $this->configureMetricFamilyForAttribute('a_metric', 'Length');
+
+        $productNullData = $this->createProductWithStandardValues(
+            $family,
+            'product_full',
+            [
+                'values' => [
+                    'a_metric' => [
+                        [
+                            'locale' => null,
+                            'scope'  => null,
+                            'data'   => null,
+                        ],
+                    ],
+                ],
+            ]
+        );
+        $this->assertNotComplete($productNullData);
+
+        $productNullAmount = $this->createProductWithStandardValues(
+            $family,
+            'product_full',
+            [
+                'values' => [
+                    'a_metric' => [
+                        [
+                            'locale' => null,
+                            'scope'  => null,
+                            'data'   => ['amount' => null, 'unit' => 'METER'],
+                        ],
+                    ],
+                ],
+            ]
+        );
+        $this->assertNotComplete($productNullAmount);
+
+        $productNullAmountAndUnit = $this->createProductWithStandardValues(
+            $family,
+            'product_full',
+            [
+                'values' => [
+                    'a_metric' => [
+                        [
+                            'locale' => null,
+                            'scope'  => null,
+                            'data'   => ['amount' => null, 'unit' => null],
+                        ],
+                    ],
+                ],
+            ]
+        );
+        $this->assertNotComplete($productNullAmountAndUnit);
+    }
+
+    private function configureMetricFamilyForAttribute($code, $metricFamily)
+    {
+        $metric = $this->get('pim_catalog.repository.attribute')->findOneByIdentifier($code);
+        $metric->setMetricFamily($metricFamily);
+        $this->get('pim_catalog.saver.attribute')->save($metric);
     }
 }
