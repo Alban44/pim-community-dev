f020eed934458fc577f305e1e88d4b72630a3e5c
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2017-02-27 09:31:43 +0100

    TIP-694: tests for options

--- src/Pim/Bundle/CatalogBundle/tests/integration/Completeness/AttributeType/OptionsAttributeTypeCompletenessIntegration.php
+++ src/Pim/Bundle/CatalogBundle/tests/integration/Completeness/AttributeType/OptionsAttributeTypeCompletenessIntegration.php
@@ -3,6 +3,7 @@
 namespace Pim\Bundle\CatalogBundle\tests\integration\Completeness\AttributeType;
 
 use Pim\Bundle\CatalogBundle\tests\integration\Completeness\AbstractCompletenessPerAttributeTypeIntegration;
+use Pim\Component\Catalog\AttributeTypes;
 
 /**
  * Checks that the completeness has been well calculated for multi select attribute type.
@@ -13,8 +14,116 @@ use Pim\Bundle\CatalogBundle\tests\integration\Completeness\AbstractCompleteness
  */
 class OptionsAttributeTypeCompletenessIntegration extends AbstractCompletenessPerAttributeTypeIntegration
 {
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->createFamilyWithRequirement(
+            'another_family',
+            'ecommerce',
+            'a_multi_select',
+            AttributeTypes::OPTION_MULTI_SELECT
+        );
+
+        $aMultiSelect = $this->get('pim_catalog.repository.attribute')->findOneByIdentifier('a_multi_select');
+
+        $redOption = $this->get('pim_catalog.factory.attribute_option')->create();
+        $redOption->setCode('red_option');
+        $redOption->setAttribute($aMultiSelect);
+
+        $greenOption = $this->get('pim_catalog.factory.attribute_option')->create();
+        $greenOption->setCode('green_option');
+        $greenOption->setAttribute($aMultiSelect);
+
+        $blueOption = $this->get('pim_catalog.factory.attribute_option')->create();
+        $blueOption->setCode('blue_option');
+        $blueOption->setAttribute($aMultiSelect);
+
+        $optionSaver = $this->get('pim_catalog.saver.attribute_option');
+        $optionSaver->save($redOption);
+        $optionSaver->save($greenOption);
+        $optionSaver->save($blueOption);
+    }
+
     public function testCompleteOptions()
     {
-        //
+        $family = $this->get('pim_catalog.repository.family')->findOneByIdentifier('another_family');
+
+        $productComplete = $this->createProductWithStandardValues(
+            $family,
+            'product_complete',
+            [
+                'values' => [
+                    'a_multi_select' => [
+                        [
+                            'locale' => null,
+                            'scope'  => null,
+                            'data'   => ['red_option', 'green_option'],
+                        ],
+                    ],
+                ],
+            ]
+        );
+        $this->assertComplete($productComplete);
+
+        $productCompleteOneOption = $this->createProductWithStandardValues(
+            $family,
+            'product_complete_one_option',
+            [
+                'values' => [
+                    'a_multi_select' => [
+                        [
+                            'locale' => null,
+                            'scope'  => null,
+                            'data'   => ['blue_option'],
+                        ],
+                    ],
+                ],
+            ]
+        );
+        $this->assertComplete($productCompleteOneOption);
+    }
+
+    public function testNotCompleteOptions()
+    {
+        $family = $this->get('pim_catalog.repository.family')->findOneByIdentifier('another_family');
+
+        $productDataEmptyArray = $this->createProductWithStandardValues(
+            $family,
+            'product_data_empty_array',
+            [
+                'values' => [
+                    'a_multi_select' => [
+                        [
+                            'locale' => null,
+                            'scope'  => null,
+                            'data'   => [],
+                        ],
+                    ],
+                ],
+            ]
+        );
+        $this->assertNotComplete($productDataEmptyArray);
+
+//        TODO: This cannot work now, but will on TIP-613. Test is added now so it will not forgotten.
+//        $productDataNull = $this->createProductWithStandardValues(
+//            $family,
+//            'product_data_null',
+//            [
+//                'values' => [
+//                    'a_multi_select' => [
+//                        [
+//                            'locale' => null,
+//                            'scope'  => null,
+//                            'data'   => null,
+//                        ],
+//                    ],
+//                ],
+//            ]
+//        );
+//        $this->assertNotComplete($productDataNull);
     }
 }
